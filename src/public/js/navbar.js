25892e8ccd56bccbd9290b5cd18f4824a369b463
phelipe <phelipe_teles@hotmail.com>
2020-07-02 16:49:42 -0300

    Add arrow up/down to show game options

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,11 +1,13 @@
-const downArrow = document.querySelector(".down-arrow");
+const arrow = document.querySelector("[arrow]");
 const gameOptions = document.querySelector(".game-options-menu");
 
-downArrow.addEventListener("click", () => {
+arrow.addEventListener("click", () => {
   const display = gameOptions.style.display;
   if (display === "none") {
     gameOptions.style.display = "flex";
+    arrow.setAttribute("arrow", "up");
   } else {
     gameOptions.style.display = "none";
+    arrow.setAttribute("arrow", "down");
   }
 })
