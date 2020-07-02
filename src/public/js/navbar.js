acc0b2ccec4ff8bdcc2eedb41e4212789aa0e8ac
phelipe <phelipe_teles@hotmail.com>
2020-07-02 20:37:11 -0300

    Put custom dificulty inputs in a div below

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,15 +1,9 @@
-const arrow = document.querySelector("[arrow]");
-const gameOptions = document.querySelector(".game-options-menu");
+const arrow = document.querySelector(".arrow");
+const gameOptions = document.querySelector(".game-options");
 
 arrow.addEventListener("click", () => {
-  const display = gameOptions.style.display;
-  if (display === "none") {
-    gameOptions.style.display = "";
-    arrow.setAttribute("arrow", "up");
-  } else {
-    gameOptions.style.display = "none";
-    arrow.setAttribute("arrow", "down");
-  }
+  gameOptions.toggleAttribute("hidden");
+  arrow.toggleAttribute("closed");
 })
 
 arrow.addEventListener("mousedown", e => {
