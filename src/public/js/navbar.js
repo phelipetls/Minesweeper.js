2471a827153b2820bc5ec7bc8a4e5bde055b35d1
phelipe <phelipe_teles@hotmail.com>
2020-07-02 18:44:48 -0300

    Just inline style none display to show game options

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -4,7 +4,7 @@ const gameOptions = document.querySelector(".game-options-menu");
 arrow.addEventListener("click", () => {
   const display = gameOptions.style.display;
   if (display === "none") {
-    gameOptions.style.display = "flex";
+    gameOptions.style.display = "";
     arrow.setAttribute("arrow", "up");
   } else {
     gameOptions.style.display = "none";
