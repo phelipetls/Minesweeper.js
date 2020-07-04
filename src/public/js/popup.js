6dfec43c31dd4d637c7b3710e0e97c45f574dc1e
phelipe <phelipe_teles@hotmail.com>
2020-07-04 15:05:24 -0300

    Move insertCenteredPopup function from popup.js to utils.js

--- src/public/js/popup.js
+++
@@ -1,8 +0,0 @@
-export function insertCenteredPopup(container, popup) {
-  const coords = container.getBoundingClientRect();
-  container.append(popup);
-  container.style.position = "relative";
-  popup.style.position = "absolute";
-  popup.style.left = (coords.width - popup.clientWidth) / 2 + "px";
-  popup.style.top = (coords.height - popup.clientHeight) / 2 + "px";
-}
--- src/public/js/utils.js
+++ src/public/js/utils.js
@@ -10,3 +10,12 @@ export function createTable(width, height) {
   const tableCells = `<td class="square"></td>`.repeat(width);
   return `<tr>${tableCells}</tr>`.repeat(height);
 }
+
+export function insertCenteredPopup(container, popup) {
+  const coords = container.getBoundingClientRect();
+  container.append(popup);
+  container.style.position = "relative";
+  popup.style.position = "absolute";
+  popup.style.left = (coords.width - popup.clientWidth) / 2 + "px";
+  popup.style.top = (coords.height - popup.clientHeight) / 2 + "px";
+}
