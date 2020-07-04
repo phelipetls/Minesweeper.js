dd1dba48725a7be0beb33c1b711a43e9daa8518d
phelipe <phelipe_teles@hotmail.com>
2020-07-04 09:24:03 -0300

    Remove sqlite3 related code in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
-const sqlite3 = require("sqlite3").verbose();
 const nunjucks = require("nunjucks");
 
 const app = express();
@@ -17,30 +16,7 @@ app.use(express.static("src/public"));
 app.use(express.json());
 app.use(morgan("short"));
 
-const db = new sqlite3.Database("./db/minesweeper.db");
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS games (
-    id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    width INTEGER NOT NULL,
-    height INTEGER NOT NULL,
-    bombs INTEGER NOT NULL,
-    time REAL NOT NULL,
-    victory INTEGER NOT NULL
-)`);
-
-
 app.get("/", (req, res) => {
-app.post("/", (req, res) => {
-  db.serialize(() => {
-    db.run(
-      `INSERT INTO games (name, width, height, bombs, time, victory) VALUES (?, ?, ?, ?, ?, ?)`,
-      Object.values(req.body)
-    );
-  });
-
-  res.sendStatus(200);
   res.render("index.html");
 });
 
