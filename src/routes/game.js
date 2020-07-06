822715ebcff235060a3bb030fa862509ee94e009
phelipe <phelipe_teles@hotmail.com>
2020-07-05 22:53:58 -0300

    Fix POST requests to /game hanging
    It was working but then it started to hang, which makes sense because I
    didn't finish the request response cycle in the if block. Now I send a
    200 status code.
    
    Also, send 401 UNAUTHORIZED status code in case there is no `req.user`.

--- src/routes/game.js
+++ src/routes/game.js
@@ -6,17 +6,19 @@ router.get("/", (req, res) => {
   res.render("game.html");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   if (req.user) {
     const { width, height, bombs, level, time, victory } = req.body;
     const { id } = req.user;
-    const { rows } = await db.query(`
+    db.query(`
       INSERT INTO plays (user_id, width, height, level, bombs, time, victory)
       VALUES ($1, $2, $3, $4, $5, $6, $7)
       `, [id, width, height, level, bombs, time, victory]
     );
+
+    res.status(200).end();
   } else {
-    res.status(200).send("Nothing to be done");
+    res.status(401).end();
   }
 });
 
