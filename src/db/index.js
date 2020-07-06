4ec5cce102b793ca6ddaed30361bc7aaf3337371
phelipe <phelipe_teles@hotmail.com>
2020-07-06 20:21:28 -0300

    Don't run dotenv config in production

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 function getConnectionString() {
   if (process.env.NODE_ENV === "production") {
     return process.env.DATABASE_URL;
   } else {
+    require("dotenv").config();
     return `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
   }
 }
