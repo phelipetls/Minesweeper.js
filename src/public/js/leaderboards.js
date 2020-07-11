4ab0e46e7d3ce550c1e4792d292f5affe75f37dd
phelipe <phelipe_teles@hotmail.com>
2020-07-11 16:08:32 -0300

    Change leaderboards page
    Allow user to query for top N players, by difficulty and by some set of
    date intervals. It won't get fancier than that, because I was going very
    crazy about all the possibilities and the pagination stuff is really
    hard to optimize.

--- src/public/js/leaderboards.js
+++ src/public/js/leaderboards.js
@@ -1,31 +1,30 @@
-const selectInterval = document.querySelector("select#interval");
 const selectDifficulty = document.querySelector("select#difficulty");
+const selectDateInterval = document.querySelector("select#date-interval");
 
-const tableContainer = document.querySelector(".leaderboard-table-container");
+const inputDateOffset = document.querySelector("input#date-offset");
+const inputTopPlayers = document.querySelector("input#top-players");
+
 const table = document.querySelector("table");
 
-const queryMore = document.querySelector(".leaderboard-query-more");
 const notFound = document.querySelector(".not-found");
 
-document.querySelector("main").addEventListener("change", e => {
-  if (e.target.tagName === "SELECT") {
-    return debounce(fetchFirstTenRows(), 250);
-  }
-});
+getLeaderboard();
+document.addEventListener("change", getLeaderboard);
 
-queryMore.addEventListener("click", () => fetchMoreRows());
-
-async function fetchLeaderboard(page = 1) {
+async function getLeaderboardRows() {
   const difficulty = selectDifficulty.value;
-  const interval = selectInterval.value;
+  const dateInterval = selectDateInterval.value;
+  const dateOffset = inputDateOffset.value;
+  const topPlayers = inputTopPlayers.value;
 
   const response = await fetch("/leaderboards", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       difficulty,
-      interval,
-      page
+      dateInterval,
+      dateOffset,
+      topPlayers,
     })
   });
 
@@ -34,35 +33,16 @@ async function fetchLeaderboard(page = 1) {
   return rows;
 }
 
-async function fetchFirstTenRows() {
-  const rows = await fetchLeaderboard();
+async function getLeaderboard() {
+  const rows = await getLeaderboardRows();
 
   if (rows.length) {
     table.removeAttribute("hidden");
     writeLeaderboardTable(rows);
-    queryMore.dataset.page = 1;
-
     notFound.setAttribute("hidden", "");
-    if (rows.length === 10) {
-      queryMore.removeAttribute("hidden", "");
-    }
   } else {
     table.setAttribute("hidden", "");
-    queryMore.setAttribute("hidden", "");
     notFound.removeAttribute("hidden");
-  }
-}
-
-async function fetchMoreRows() {
-  const page = ++queryMore.dataset.page;
-  const rows = await fetchLeaderboard(page);
-
-  if (rows.length) {
-    appendToLeaderboard(rows, page);
-  }
-
-  if (rows.length < 10) {
-    queryMore.setAttribute("hidden", "");
   }
 }
 
@@ -72,15 +52,6 @@ function writeLeaderboardTable(rows) {
     html += getRowHTML(row);
   }
   table.tBodies[0].innerHTML = html;
-}
-
-function appendToLeaderboard(rows) {
-  let html = "";
-  console.log(rows);
-  for (const row of rows) {
-    html = getRowHTML(row);
-    table.tBodies[0].insertAdjacentHTML("beforeend", html);
-  }
 }
 
 function getRowHTML(row) {
@@ -93,13 +64,3 @@ function getRowHTML(row) {
     "</tr>"
   );
 }
-
-function debounce(func, ms) {
-  let timeout;
-  return function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, arguments), ms);
-  };
-}
-
-fetchFirstTenRows();
--- src/routes/leaderboards.js
+++ src/routes/leaderboards.js
@@ -9,25 +9,29 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const { difficulty, interval, page } = req.body;
+    const { difficulty, dateInterval, dateOffset, topPlayers } = req.body;
+
+    let fullDateInterval =
+      dateInterval === "all-time"
+        ? "100 years"
+        : `${dateOffset} ${dateInterval}`;
 
     const result = await db.query({
       text: `
       SELECT users.name AS Player,
+             plays.time AS Time,
              plays.width AS Width,
              plays.height AS Height,
-             plays.bombs AS Bombs,
-             plays.time AS Time
+             plays.bombs AS Bombs
       FROM users JOIN plays ON (users.id = plays.user_id)
       WHERE plays.victory IS TRUE
             AND plays.level = $1
             AND (plays.date BETWEEN CURRENT_DATE - $2::interval AND CURRENT_DATE)
-      ORDER BY time DESC
-      LIMIT 10
-      OFFSET 10 * ($3 - 1)
+      ORDER BY time ASC
+      LIMIT $3
       ;
     `,
-      values: [difficulty, interval, page]
+      values: [difficulty, fullDateInterval, topPlayers]
     });
 
     res.send(result.rows);
